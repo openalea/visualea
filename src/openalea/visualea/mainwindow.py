57d32b978f8757b87a6f30571652cd05c42c869f
pradal <christophe.pradal@inria.fr>
2023-02-16 19:18:37 +0100

    Update copyright

--- src/openalea/visualea/mainwindow.py
+++ src/openalea/visualea/mainwindow.py
@@ -2,7 +2,7 @@
 #
 #       OpenAlea.Visualea: OpenAlea graphical user interface
 #
-#       Copyright 2006-2009 INRIA - CIRAD - INRA
+#       Copyright 2006-2023 INRIA - CIRAD - INRAE
 #
 #       File author(s): Samuel Dufour-Kowalski <samuel.dufour@sophia.inria.fr>
 #                       Christophe Pradal <christophe.prada@cirad.fr>
